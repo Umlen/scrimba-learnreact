1c1c7525bac960ba4961114e76573288e6f44094
Viktor <victorpatrin@gmail.com>
2022-06-28 17:17:49 +0700

    components

--- react-facts/index.js
+++ react-facts/index.js
@@ -1,29 +1,35 @@
-const logo = (
-    <img width = '40px' src = './react-logo.png' alt = 'react-logo' />
-);
+const Logo = () => {
+    return (
+        <img width = '40px' src = './react-logo.png' alt = 'react-logo' />
+    );
+};
 
-const header = (
-  <h1>Fun facts about React</h1>  
-);
+const Header = () => {
+    return (
+        <h1>Fun facts about React</h1>  
+    );
+};
 
-const list = (
-    <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>    
-);
+const List = () => {
+    return (
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>    
+    );
+};
 
 const Page = () => {
     return (
         <main>
-            {logo}
-            {header}
-            {list}
+            <Logo />
+            <Header />
+            <List />
         </main>
     );
-}
+};
 
 ReactDOM.render(<Page />, document.getElementById('root'));
