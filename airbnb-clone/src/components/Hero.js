3d324d8dba020c56723bf63339358148af40e384
Viktor <victorpatrin@gmail.com>
2022-07-11 18:27:58 +0700

    hero section

---
+++ airbnb-clone/src/components/Hero.js
@@ -0,0 +1,19 @@
+import photoGrid from '../images/photo-grid.png';
+
+const Hero = () => {
+    return (
+        <section className = 'hero'>
+            <img 
+                src = {photoGrid} 
+                alt = 'photo grid' 
+                className = 'hero--img'
+            />
+            <h1 className = 'hero--header'>Online Experiences</h1>
+            <p className = 'hero--text'>
+                Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+        </section>
+    );
+};
+
+export default Hero;
